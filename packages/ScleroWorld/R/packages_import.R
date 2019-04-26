fd252caddb78ec60be73ac9a2219e2b069d7374f
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-04-26 00:00:21 -0500

    package for this project

--- packages/ScleroWorld/R/packages_import.R
+++ packages/ScleroWorld/R/packages_import.R
@@ -5,15 +5,24 @@
 #----------------------------------------------------------------------------
 
 #' @import logging
+#' @import assertr
 #' @import rticles
-#' @import tidyverse
+#' @import ggplot2
+#' @import dplyr
 #' @import bookdown
 #' @import ggforce
 #' @import ggridges
 #' @import ggrepel
 #' @import readxl
-#' @import ggtree
+#' @import huxtable
+#' @import rmarkdown
+#' @import agricolae
+#' @import ggraph
+#' @import cowplot
+#' @import viridis
+#' @import adegenet
+#' @import ggcompoplot
+#' @import visNetwork
+#' @import extrafont
 NULL
-
-
 
